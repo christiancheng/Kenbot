8b481691b8e97d1154f2d3876e3cd058b7ad49f2
Joshua Ramos <jdr023@ucsd.edu>
2015-09-01 11:20:31 -0700

    merge

--- datareader.py
+++ datareader.py
@@ -1,62 +1,30 @@
 import csv
-# from os.path import expanduser
-# home = expanduser("~")
+from os.path import expanduser
+home = expanduser("~")
 
 from collections import namedtuple
 
-Person = namedtuple('person','name,sex,role,blackhair,glasses,basketball')
+Person = namedtuple('person','name,sex,role,blackhair,glasses,basketball,track, \
+                    noSports,noFacebook,playedLeague,usesiPhone,haveSiblings')
 People = []
 
-with open('survey.csv', 'rb') as csvfile:
+with open(home+'/Downloads/survey.csv', 'rb') as csvfile:
 
     datareader = csv.reader(csvfile)
     firstline = True
     for row in datareader:
-        if firstline:
+        if firstline: #ignores the first row of the csv
             firstline = False
         else:
-            p = Person(row[1],row[2],row[3],row[12],row[11],row[13])
+            p = Person(row[1],row[2],row[3],row[12],row[11],row[13],row[14], \
+                       row[15],row[16],row[17],row[18],row[19])
             People.append(p)
 
 if __name__ == '__main__':
     print People
 
-##spis student
-##mentor
-##prof
+'''column 14 is track'''
 
-##glasses
-##black hair
-##basketball
-##track or cross
-##no sports
-##no facebook
-##played league
-##uses iphone
-##has siblings
-
-'''college'''
-##marshall
-##warren
-##revelle
-##sixth
-##erc
-##muir
-##not a student or mentor
-
-'''mentors only'''
-##second year
-##third year
-##fourth year
-##fifth year
-##sixth year
-##seventh
-
-'''students major'''
-##cs
-##ce
-##undeclared
-##other
 
 
 
